d53caaae522d61de7259bfcb85f9ccac77c6e132
John DaCorte <john.dacorte@gmail.com>
2024-03-01 17:35:50 -0500

    Initial Commit

---
+++ GolfEventScorer2.2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GolfEventScorer2.2
+//
+//  Created by John DaCorte on 3/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GolfEventScorer2.2/GolfEventScorer2_2App.swift
@@ -0,0 +1,17 @@
+//
+//  GolfEventScorer2_2App.swift
+//  GolfEventScorer2.2
+//
+//  Created by John DaCorte on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct GolfEventScorer2_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
